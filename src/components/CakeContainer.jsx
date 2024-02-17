a56fe5da6889c1bae6336f94ae23d5a5b2e534d5
anand-bits <h20220277@pilani.bits-pilani.ac.in>
2024-02-18 01:46:07 +0530

    Action, reducer Made

---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,15 @@
+import react from 'react'
+
+function CakeContainer()
+{
+
+    return (
+        <div>
+           <h2>
+            Number of Cake </h2> 
+            <button> Buy Cake</button>
+        </div>
+    )
+}
+
+export default CakeContainer;
